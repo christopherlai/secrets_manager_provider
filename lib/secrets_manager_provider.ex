6d8ed27c95d50cb59596e75f1ccb7757d451376b
Humber Aquino <humberaquino@gmail.com>
2019-10-07 11:45:01 -0300

    Add extra config support to allow setting aws secret name via env var

--- lib/secrets_manager_provider.ex
+++ lib/secrets_manager_provider.ex
@@ -1,6 +1,6 @@
 defmodule SecretsManagerProvider do
   @moduledoc """
-  AWS Secrets Manager ConfigProvider for Elixir Releases.  
+  AWS Secrets Manager ConfigProvider for Elixir Releases.
   """
 
   @behaviour Config.Provider
@@ -9,22 +9,49 @@ defmodule SecretsManagerProvider do
   alias SecretsManagerProvider.ExAwsClient
 
   @impl true
-  def init(path) when is_binary(path), do: path
+  def init(args), do: args
 
   @impl true
-  def load(config, path) do
+  def load(config, args) do
     {:ok, _deps} = Application.ensure_all_started(:hackney)
     {:ok, _deps} = Application.ensure_all_started(:ex_aws)
 
     client = Application.get_env(:secrets_manager_provider, :client, ExAwsClient)
     parser = Application.get_env(:secrets_manager_provider, :parser, Toml)
 
-    secrets =
-      path
-      |> client.get_secrets()
-      |> parser.decode!()
-      |> to_keyword()
+    secrets = extract_secrets(client, parser, args)
 
     Config.Reader.merge(config, secrets)
   end
+
+  defp extract_secrets(client, parser, path) when is_binary(path) do
+    extract_secrets_from_path(client, parser, path)
+  end
+
+  defp extract_secrets(client, parser, keywords) when is_list(keywords) do
+    secret_type = detect_secret_type(keywords)
+    extract_secrets_from_type(client, parser, keywords, secret_type)
+  end
+
+  defp extract_secrets_from_type(client, parser, keywords, :env_var) do
+    var_name = Keyword.get(keywords, :env_var)
+    path = System.get_env(var_name)
+    extract_secrets_from_path(client, parser, path)
+  end
+
+  defp extract_secrets_from_type(client, parser, keywords, :path) do
+    path = Keyword.get(keywords, :path)
+    extract_secrets_from_path(client, parser, path)
+  end
+
+  defp extract_secrets_from_path(client, parser, path) when is_binary(path) do
+    path
+    |> client.get_secrets()
+    |> parser.decode!()
+    |> to_keyword()
+  end
+
+  defp detect_secret_type(env_var: _var_name), do: :env_var
+  defp detect_secret_type(path: _var_name), do: :path
+  defp detect_secret_type(_), do: nil
 end
---
+++ test/secrets_manager_provider_keywords_test.exs
@@ -0,0 +1,70 @@
+defmodule SecretsManagerProviderKeywordsTest do
+  use ExUnit.Case, async: true
+
+  import Mox
+
+  setup :verify_on_exit!
+
+  describe "init/1" do
+    test "returns a given keywords" do
+      assert SecretsManagerProvider.init(env_var: "SOME_VAR") == [env_var: "SOME_VAR"]
+    end
+  end
+
+  describe "load/2 with TOML using env var" do
+    setup do
+      System.put_env("SOME_VAR", "aws/secret/keystore")
+      Application.put_env(:secrets_manager_provider, :client, SecretsManagerProvider.MockClient)
+      Application.put_env(:secrets_manager_provider, :parser, Toml)
+
+      SecretsManagerProvider.MockClient
+      |> expect(:get_secrets, fn _path ->
+        """
+        [toplevel]
+        sublevel = "config"
+        """
+      end)
+
+      expected = [toplevel: [sublevel: "config"]]
+
+      {:ok, %{expected: expected}}
+    end
+
+    test "returns keyword configurations from env var", %{expected: expected} do
+      assert SecretsManagerProvider.load([], env_var: "SOME_VAR") == expected
+    end
+
+    test "returns merged keyword configurations", %{expected: expected} do
+      assert SecretsManagerProvider.load([toplevel: [sublevel: false]], env_var: "SOME_VAR") ==
+               expected
+    end
+  end
+
+  describe "load/2 with TOML using path keyword" do
+    setup do
+      Application.put_env(:secrets_manager_provider, :client, SecretsManagerProvider.MockClient)
+      Application.put_env(:secrets_manager_provider, :parser, Toml)
+
+      SecretsManagerProvider.MockClient
+      |> expect(:get_secrets, fn _path ->
+        """
+        [toplevel]
+        sublevel = "config"
+        """
+      end)
+
+      expected = [toplevel: [sublevel: "config"]]
+
+      {:ok, %{expected: expected}}
+    end
+
+    test "returns keyword configurations from env var", %{expected: expected} do
+      assert SecretsManagerProvider.load([], path: "/some/path") == expected
+    end
+
+    test "returns merged keyword configurations", %{expected: expected} do
+      assert SecretsManagerProvider.load([toplevel: [sublevel: false]], path: "/some/path") ==
+               expected
+    end
+  end
+end
