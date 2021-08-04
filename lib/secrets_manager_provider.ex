47256f9869957a7358a90840e27a5d39c044867e
Felipe Vieira <nvieirafelipe@gmail.com>
2021-08-04 18:47:03 +0200

    Receive config as init args

--- lib/secrets_manager_provider.ex
+++ lib/secrets_manager_provider.ex
@@ -15,18 +15,15 @@ defmodule SecretsManagerProvider do
   def load(config, args) do
     {:ok, _deps} = Application.ensure_all_started(:hackney)
     {:ok, _deps} = Application.ensure_all_started(:ex_aws)
-    configuration = Configuration.new()
+    configuration = Configuration.new(args)
 
-    args
-    |> get_path()
+    configuration
+    |> Map.get(:name)
     |> configuration.client.get_secrets()
     |> configuration.parser.decode!()
     |> to_keyword()
     |> merge_configs(config)
   end
-
-  defp get_path({:env, name}), do: System.get_env(name)
-  defp get_path({:name, path}), do: path
 
   defp merge_configs(secrets, config) do
     Config.Reader.merge(config, secrets)
